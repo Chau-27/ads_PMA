7a6145330106f92cf7e60d029a3439b3f88d15f5
Chen Li Chau <chenlichau@live.utm.my>
2025-06-21 19:20:33 +0800

    Model Traninig

---
+++ model/train_model.py
@@ -0,0 +1,26 @@
+import pandas as pd
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.pipeline import Pipeline
+import joblib
+
+
+# Load data
+df = pd.read_csv("Model_Data_Input.csv")
+X = df[["LIMIT_BAL","GENDER","EDUCATION_POSTGRAD","EDUCATION_GRADUATE","EDUCATION_HIGHSCHOOL","MARRIAGE_MARRIED",
+        "MARRIAGE_SINGLE","AGE","PAY_STATUS_SEP05","PAY_STATUS_AUG05","PAY_STATUS_JUL05","PAY_STATUS_JUN05",
+        "PAY_STATUS_MAY05","PAY_STATUS_APR05","BILL_AMT_SEP05","BILL_AMT_AUG05","BILL_AMT_JUL05","BILL_AMT_JUN05",
+        "BILL_AMT_MAY05","BILL_AMT_APR05","PAY_AMT_SEP05","PAY_AMT_AUG05","PAY_AMT_JUN05","PAY_AMT_JUL05",
+        "PAY_AMT_JUN05","PAY_AMT_MAY05"]]
+y = df["DEFAULT_NEXT_MONTH"]
+
+# Pipeline
+model = Pipeline([
+    ("regressor", DecisionTreeClassifier())
+])
+
+# Train
+model.fit(X, y)
+
+# Save model
+joblib.dump(model, "default_model.pkl")
+print("Model saved as default_model.pkl")
