a7c82e1598c7ebcca3deed972c37b16dccaea4e8
Chen Li Chau <chenlichau@live.utm.my>
2025-06-26 15:48:12 +0800

    Fix train model

--- model/train_model.py
+++ model/train_model.py
@@ -1,5 +1,5 @@
 import pandas as pd
-from sklearn.tree import DecisionTreeClassifier
+from sklearn.linear_model import LogisticRegression
 from sklearn.pipeline import Pipeline
 import joblib
 
@@ -11,13 +11,13 @@ df = pd.read_csv("Model_Data_Input.csv")
 X = df[["LIMIT_BAL","GENDER","EDUCATION_POSTGRAD","EDUCATION_GRADUATE","EDUCATION_HIGHSCHOOL","MARRIAGE_MARRIED",
         "MARRIAGE_SINGLE","AGE","PAY_STATUS_SEP05","PAY_STATUS_AUG05","PAY_STATUS_JUL05","PAY_STATUS_JUN05",
         "PAY_STATUS_MAY05","PAY_STATUS_APR05","BILL_AMT_SEP05","BILL_AMT_AUG05","BILL_AMT_JUL05","BILL_AMT_JUN05",
-        "BILL_AMT_MAY05","BILL_AMT_APR05","PAY_AMT_SEP05","PAY_AMT_AUG05","PAY_AMT_JUN05","PAY_AMT_JUL05",
-        "PAY_AMT_JUN05","PAY_AMT_MAY05"]]
+        "BILL_AMT_MAY05","BILL_AMT_APR05","PAY_AMT_SEP05","PAY_AMT_AUG05","PAY_AMT_JUL05","PAY_AMT_JUN05",
+        "PAY_AMT_MAY05","PAY_AMT_APR05"]]
 y = df["DEFAULT_NEXT_MONTH"]
 
 # Pipeline
 model = Pipeline([
-    ("classifier", DecisionTreeClassifier())
+    ("Regression", LogisticRegression(max_iter=500))
 ])
 
 # Train
@@ -26,3 +26,6 @@ model.fit(X, y)
 # Save model
 joblib.dump(model, "default_model.pkl")
 print("Model saved as default_model.pkl")
+
+# Print feature names
+print(model.named_steps['Regression'].feature_names_in_)
