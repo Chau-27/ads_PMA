749de4843ef0ecd009bef66e7b537a2b0ba2c3e0
Chen Li Chau <chenlichau@live.utm.my>
2025-07-11 20:47:54 +0800

    modify test

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,10 +1,8 @@
 # tests/test_main.py 
 import sys 
 import os 
-# Add the src directory to the Python module search path 
-sys.path.insert(0, 
-os.path.abspath(os.path.join(os.path.dirname(__file__), '../src'))) 
-from main import load_data 
+
+from main.Dashboard import load_data 
 def test_load_data(): 
     df = load_data()
     print(f"DataFrame shape: {df.shape}")  # Debugging information 
