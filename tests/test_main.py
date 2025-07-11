90d7033a13621f2ce446faa2de3cb41e7f3c3b57
Chen Li Chau <chenlichau@live.utm.my>
2025-07-11 20:52:03 +0800

    fix test

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,9 +1,7 @@
 # tests/test_main.py 
-import sys 
-import os 
 
-from main.Dashboard import load_data 
+from Dashboard.dashboard_model import load_data 
 def test_load_data(): 
     df = load_data()
     print(f"DataFrame shape: {df.shape}")  # Debugging information 
-    assert not df.empty, "DataFrame should not be empty" 
+    assert not df.empty, "DataFrame should not be empty"
