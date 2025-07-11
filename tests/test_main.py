a5ae3d95bc475e4ecbe24151362ac96a62f0c2b3
Chen Li Chau <chenlichau@live.utm.my>
2025-07-11 20:55:07 +0800

    fix test 2

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,6 +1,10 @@
 # tests/test_main.py 
 
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from Dashboard.dashboard_model import load_data 
+
 def test_load_data(): 
     df = load_data()
     print(f"DataFrame shape: {df.shape}")  # Debugging information 
